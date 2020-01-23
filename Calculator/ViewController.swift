66f87898ca0b9c3219c6a8e1fc32ae1f63503873
HS <sekhah@my.lanecc.edu>
2020-01-22 16:34:07 -0800

    Number button and zero methods done.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,21 +1,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var displayLabel: UILabel!
+    
+    let numberOfDigits = 8
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func numberButtonClicked(_ sender: UIButton) {
-        
-        
-        
-        
+        if displayLabel.text == "Stack Empty" {
+            displayLabel.text = sender.titleLabel!.text
+        } else {
+            if displayLabel.text!.count < 8 {
+                displayLabel.text! += sender.titleLabel!.text!
+            }
+        }
     }
+    
     @IBAction func zeroButtonClicked(_ sender: UIButton) {
-        
+        if displayLabel.text != "Stack Empty" {
+            if displayLabel.text!.count < 8 {
+                displayLabel.text! += "0"
+            }
+        }
         
     }
     
