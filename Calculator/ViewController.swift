4edf315a75a912c69f49fd5b62ab00010e35da59
HS <sekhah@my.lanecc.edu>
2020-01-22 17:03:04 -0800

    Enter button method done. Added flag for enter button pressed or not.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -5,39 +5,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
     
     let numberOfDigits = 8
+    var stack = [Int]()
+    var stackEntryJustMade = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func numberButtonClicked(_ sender: UIButton) {
-        if displayLabel.text == "Stack Empty" {
+        if displayLabel.text == "Stack Empty" || stackEntryJustMade {
             displayLabel.text = sender.titleLabel!.text
-        } else {
-            if displayLabel.text!.count < 8 {
-                displayLabel.text! += sender.titleLabel!.text!
-            }
+            stackEntryJustMade = false
+        } else if displayLabel.text!.count < 8 {
+            displayLabel.text! += sender.titleLabel!.text!
         }
     }
     
     @IBAction func zeroButtonClicked(_ sender: UIButton) {
-        if displayLabel.text != "Stack Empty" {
-            if displayLabel.text!.count < 8 {
+        if !stackEntryJustMade {
+            if displayLabel.text != "Stack Empty" && displayLabel.text!.count < 8 {
                 displayLabel.text! += "0"
             }
         }
-        
     }
     
     @IBAction func enterButtonClicked(_ sender: UIButton) {
+        if displayLabel.text != "Stack Empty" {
+            stack.append(Int(displayLabel.text!)!)
+            stackEntryJustMade = true
+        }
     }
     
-    
     @IBAction func operationButtonClicked(_ sender: UIButton) {
-        
-        
         
     }
     
 }
-
